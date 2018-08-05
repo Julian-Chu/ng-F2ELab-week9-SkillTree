705972cd39ce7613f6f3ed78123212f06a9ef6ec
JulianChu <yulang.chu@gmail.com>
2018-08-06 00:02:52 +0200

    fix assets path

--- src/app/current-rank/current-rank.component.ts
+++ src/app/current-rank/current-rank.component.ts
@@ -13,32 +13,32 @@ export class CurrentRankComponent implements OnInit {
       title: "FRONT-END NOOB",
       message:
         "Welcome to aboard. Your mission is: Collect the resources and learn the skills to upgrade the ship. Good luck, captain! ",
-      src: "../assets/icons/img-ship-noob.png"
+      src: "assets/icons/img-ship-noob.png"
     },
     {
       title: "FRONT-END BEGINNER",
       message:
         "Congradulations! You become the “Front-end Beginnner”. Keep searching the resources to upgrade to the next level.",
-      src: "../assets/icons/img-ship-beginner.png"
+      src: "assets/icons/img-ship-beginner.png"
     },
     {
       title: "FRONT-END BEGINNER",
       message:
         "Congradulations! You become the “Front-end Beginnner”. Keep searching the resources to upgrade to the next level.",
-      src: "../assets/icons/img-ship-beginner.png"
+      src: "assets/icons/img-ship-beginner.png"
     },
     {
       title: "FRONT-END DEVELOPER",
       message:
         "You’re doing well! Now you’re a “Front-end Developer”. It’s close to complete the upgrading program." +
         " Next level: Front-end Master.",
-      src: "../assets/icons/img-ship-developer.png"
+      src: "assets/icons/img-ship-developer.png"
     },
     {
       title: "FRONT-END MASTER",
       message:
         "Excellent! You’re a “Front-end Master” now. But a new galary was just detected by the system. Captain, make your choice. ",
-      src: "../assets/icons/img-ship-master.png"
+      src: "assets/icons/img-ship-master.png"
     }
   ];
   constructor() {}
